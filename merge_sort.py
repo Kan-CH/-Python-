a45b5f2e63bf2c821ede8cd6704b41c84e004a74
Kan-CH <875978410@qq.com>
2017-08-30 15:27:34 +0800

    归并排序算法

---
+++ merge_sort.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list)/2
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+    return merge(left,right)
